75f62013d28269a4aad392401b93c05cff0f3168
Thomas Skerbis <thomas.skerbis@mac.com>
2025-02-20 19:37:03 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,1 +1,13 @@
-<?php // Boot code
+<?php
+// boot.php
+if (rex::isBackend() && rex::getUser()) {
+    rex_extension::register(
+        'SLICE_SHOW', 
+        ['rex_slice_extension', 'handleSliceShow']
+    );
+    
+    if (rex_be_controller::getCurrentPage() == 'content/edit') {
+        rex_view::addCssFile($this->getAssetsUrl('css/slice-ops.css'));
+        rex_view::addJsFile($this->getAssetsUrl('dist/slice-ops.js'));
+    }
+}
